39140edef04c0f8d246c56640b30daf00e5e4e7e
Cody Rains <cody.rains34@gmail.com>
2021-10-20 14:58:39 -0400

    added switch statement for the punch him response in the first encounter.

--- Program.cs
+++ Program.cs
@@ -22,14 +22,33 @@ namespace CocoTextAdventureGameOfTheYearAddition
         public static void first()
         {
             string choice;
+
             Console.WriteLine("You wake up in a tavern, clueless of the time, with a large man hovering over you. 'This is my table, and you're sitting in my seat!' he says, as spit flies out of his mouth and onto your cheek.");
             Console.WriteLine("What do you do?");
             Console.WriteLine("1. Punch him right in the kisser.");
-            Console.WriteLine("2. Tell him 'NO, this is my table now.'");
-            Console.WriteLine("3. Challenge him to an arm wrestling match. You say 'Winner takes the table!'");
-            Console.WriteLine("Choice: ");
+            Console.WriteLine("2. Blow his mind with a magic display.'");
+            Console.WriteLine("3. Pee a little bit.'");
+            Console.Write("Choice: ");
             choice = Console.ReadLine().ToLower();
             Console.Clear();
+
+            switch (choice)
+            {
+                case "1":
+                case "punch him right in the kisser":
+                case "punch him":
+                case "punch":
+                    {
+                        Console.WriteLine("You swing your fist at the drunkards face with a left hook that would even impress the likes of the great Sir Muhammad Ali himself!");
+                        Console.WriteLine("The rest of the crowd in the tavern pause in silence for a moment, until you are surprised with a roar of cheering! Those cheers are shortly followed up with a few brawls spread out through the tavern. Looks like you know how to get the people going!");
+                        Console.WriteLine("The Barkeep of the tavern comes over to investigate the source of the original dispute.");
+                        Console.WriteLine("When he sees the man knocked out cold, he grabs your hand, and raises it to the ceiling!");
+                        Console.WriteLine("Press enter to continue");
+                        Console.ReadLine();
+                        gameOver();
+                        break;
+                    }
+            }
         }
 
         public static void second()
