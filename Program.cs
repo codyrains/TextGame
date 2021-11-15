557283f8e3a6a58cca065287a52c48f508a434c7
Cody Rains <cody.rains34@gmail.com>
2021-11-15 15:19:45 -0500

    added code for dice game

--- Program.cs
+++ Program.cs
@@ -155,6 +155,8 @@ namespace CocoTextAdventureGameOfTheYearAddition
             if (secChoice == "yes" || secChoice == "y")
             {
                 // dice game method here
+                Console.WriteLine("Excellent! We will each roll a dice and whoever rolls higher wins the round! We will play 7 total rounds. Good luck!");
+                dice();
             }
             else if (secChoice == "no" || secChoice == "n")
             {
@@ -170,6 +172,66 @@ namespace CocoTextAdventureGameOfTheYearAddition
                 Console.ReadLine();
                 second();
             }
+        }
+
+        public static void dice()
+        {
+            int playerRandomNum;
+            int randyRandomNum;
+
+            int playerPoints = 0;
+            int randyPoints = 0;
+
+            Random random = new Random();
+            for (int i = 0; i < 7; i++)
+            {
+                Console.WriteLine("Press any key to roll the dice!");
+
+                Console.ReadKey();
+
+                playerRandomNum = random.Next(1, 7);
+                Console.WriteLine("You rolled a " + playerRandomNum);
+
+                Console.WriteLine("...");
+                System.Threading.Thread.Sleep(1000);
+
+                randyRandomNum = random.Next(1, 7);
+                Console.WriteLine("Randy rolled a " + randyRandomNum);
+
+                if (playerRandomNum > randyRandomNum)
+                {
+                    playerPoints++;
+                    Console.WriteLine("You win this round...");
+                }
+                else if (playerRandomNum < randyRandomNum)
+                {
+                    randyPoints++;
+                    Console.WriteLine("oooo looks like Randy gotcha this time :)");
+                }
+                else
+                {
+                    Console.WriteLine("It's a draw... Randy doesn't look too happy about that... don't look him in the eyes for too long.");
+                }
+
+                Console.WriteLine("The score is now - You : " + playerPoints + ". Randy : " + randyPoints + ".");
+                Console.WriteLine();
+            }
+
+            if (playerPoints > randyPoints)
+            {
+                Console.WriteLine("You won against Randy!! Enjoy your sweet sweet victory!");
+            }
+            else if (playerPoints < randyPoints)
+            {
+                Console.WriteLine("Uh oh...looks like Randy won the game! He'll never let you live this one down.");
+            }
+            else
+            {
+                Console.WriteLine("It's a draw!! Randy reaches his hand out for a good ol shake, but pulls it away at the last second.");
+            }
+
+            Console.ReadKey();
+            youWin();
         }
 
         public static void gameOver()
@@ -194,9 +256,5 @@ namespace CocoTextAdventureGameOfTheYearAddition
             Console.Clear();
             gameTitle();
         }
-    }
-    public class Dice
-    {
-
     }
 }
